3764ee511cff454d233690c3f4fbae1f0e58f6d7
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 19:44:40 +0100

    Create List.tsx

---
+++ DZIEN_2/zadania_ts/src/List.tsx
@@ -0,0 +1,30 @@
+import React,{FC,useEffect,memo} from "react";
+import Task from './Task'
+
+export type Todo = {
+    id:number,
+    task:string
+}
+
+interface Props {
+    todoList: Todo[]
+    handleDelete:any
+}
+
+const List:FC<Props> = ({todoList, handleDelete}) => {
+    useEffect(()=>{
+
+    })
+    return (
+        <ul>
+            {todoList.map((todo:Todo)=> (
+                <Task
+                key={todo.id}
+                id={todo.id}
+                task={todo.task}
+                handleDelete={handleDelete}/>
+                ))}
+        </ul>
+    )
+}
+export default memo(List)
