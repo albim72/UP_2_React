513976acffc598642803fd6828393a86973cfba3
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 18:42:33 +0100

    Create GreetingFunctional.tsx

---
+++ DZIEN_3/GREET_CLASS/src/GreetingFunctional.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface GreetingProps {
+    message: string
+}
+
+export default function Greeting(props: GreetingProps) {
+    console.log("Renderuję komponent Greeting")
+    
+    return(<div>
+        {props.message}
+    </div>);
+}
