3f422ae9335f8fecee054bc3c78dd18ff53d5e2d
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 18:20:33 +0100

    Create WidthInnerWidth.tsx

---
+++ DZIEN_3/SZEROKOSC/src/components/MyComponent/WidthInnerWidth.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+
+const WidthInnerWidth = (Component: any) => (props: any) => {
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+    
+    const handleResize = () => {
+        setInnerWidth(window.innerWidth)
+    }
+    
+    useEffect(()=>{
+        window.addEventListener('resize',handleResize)
+        
+        return () => {
+            window.removeEventListener('resize',handleResize)
+        }
+    },[])
+    return <Component {...props} innerWidth = {innerWidth}/>
+}
+
+export default WidthInnerWidth()
