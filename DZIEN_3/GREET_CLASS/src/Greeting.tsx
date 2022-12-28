18b9ba5d74fde11a46e27e7e264a1c2c1ca7d360
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 19:16:43 +0100

    Update Greeting.tsx

--- DZIEN_3/GREET_CLASS/src/Greeting.tsx
+++ DZIEN_3/GREET_CLASS/src/Greeting.tsx
@@ -3,24 +3,23 @@ import React from "react";
 interface GreetingProps {
     name?: string
 }
-
 interface GreetingState {
     message: string
 }
 
-export default class Greeting extends React.Component<GreetingProps, GreetingState>{
-
+export default class Greeting extends React.Component<GreetingProps, GreetingState> {
     constructor(props: GreetingProps) {
         super(props);
-        
+
         this.state = {
             message: Greeting.getNewMessage(props.name)
         }
     }
+
     state: GreetingState;
-    
-    static getDerivedStateFromProps(props: GreetingProps, state:GreetingState){
-        console.log(props,state);
+
+    static getDerivedStateFromProps(props: GreetingProps, state:GreetingState) {
+        console.log(props, state);
         if(props.name && props.name !== state.message) {
             const newState = {...state};
             newState.message = Greeting.getNewMessage(props.name);
@@ -28,14 +27,17 @@ export default class Greeting extends React.Component<GreetingProps, GreetingSta
         }
         return state;
     }
-    
-    static getNewMessage(name: string = ""){
+
+    static getNewMessage(name: string = "") {
+        return `Witaj z Greeting, ${name}`;
+    }
+
+    render() {
+        console.log("Renderuję komponent Greeting")
         if(!this.props.name) {
-            return <div>
-                Nie podano imienia!
-            </div>
+            return <div>Nie podano imienia.</div>;
         }
-        return  <div>
+        return <div>
             {this.state.message}
         </div>;
     }
