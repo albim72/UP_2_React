11c5b8ec7a4e5d1e0fa2e06f0d776b0a2029bbf1
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 20:16:14 +0100

    Update App.tsx

--- DZIEN_3/GREETINGS_HOOKS/src/App.tsx
+++ DZIEN_3/GREETINGS_HOOKS/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useReducer, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greeting from "./GreetingFunctional";
@@ -17,6 +17,25 @@ const reducer = (state: any, action: any) => {
         default:
             throw new Error("Nieprawidłowy typ akcji: " + action.type);
     }
+}
+
+const initialState = {
+    enteredName: "",
+    message: ""
+};
+
+function App() {
+    const [{message,enteredName},dispatch] = 
+    useReducer(reducer,initialState);
+    
+    const [startCount,setStartCount] = useState(0);
+    const [count, setCount] = useState(0);
+    
+    const setCountCallback = useCallback(()=> {
+        const inc = count + 1 > startCount ? count + 1: Number(count+1) + startCount;
+        setCount(inc);
+    },[count,startCount]);
+    
 }
 
 
