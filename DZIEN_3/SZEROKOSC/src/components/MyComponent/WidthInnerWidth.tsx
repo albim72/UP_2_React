0a3072e75fa0af6a5febdc552613e898605d8211
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 18:26:48 +0100

    Update WidthInnerWidth.tsx

--- DZIEN_3/SZEROKOSC/src/components/MyComponent/WidthInnerWidth.tsx
+++ DZIEN_3/SZEROKOSC/src/components/MyComponent/WidthInnerWidth.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 
 const WidthInnerWidth = (Component: any) => (props: any) => {
     const [innerWidth, setInnerWidth] = useState(window.innerWidth)
-    
+
     const handleResize = () => {
         setInnerWidth(window.innerWidth)
     }
-    
+
     useEffect(()=>{
         window.addEventListener('resize',handleResize)
         
@@ -18,4 +18,4 @@ const WidthInnerWidth = (Component: any) => (props: any) => {
     return <Component {...props} innerWidth = {innerWidth}/>
 }
 
-export default WidthInnerWidth()
+export default WidthInnerWidth
