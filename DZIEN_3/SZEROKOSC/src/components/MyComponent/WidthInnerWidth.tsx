8fbc56322b111d916ac4970e565c3a427f524140
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 18:31:22 +0100

    Update WidthInnerWidth.tsx

--- DZIEN_3/SZEROKOSC/src/components/MyComponent/WidthInnerWidth.tsx
+++ DZIEN_3/SZEROKOSC/src/components/MyComponent/WidthInnerWidth.tsx
@@ -2,15 +2,17 @@ import { useEffect, useState } from 'react';
 
 
 const WidthInnerWidth = (Component: any) => (props: any) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const [innerWidth, setInnerWidth] = useState(window.innerWidth)
 
     const handleResize = () => {
         setInnerWidth(window.innerWidth)
     }
 
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(()=>{
         window.addEventListener('resize',handleResize)
-        
+
         return () => {
             window.removeEventListener('resize',handleResize)
         }
