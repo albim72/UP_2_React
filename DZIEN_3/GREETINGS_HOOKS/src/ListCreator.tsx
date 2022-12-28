7176c542b9ce29d6f425de62acfb3c23a6426734
albim72 <marcin.albiniak72@gmail.com>
2022-12-28 19:48:55 +0100

    Create ListCreator.tsx

---
+++ DZIEN_3/GREETINGS_HOOKS/src/ListCreator.tsx
@@ -0,0 +1,31 @@
+import React, {FC,useEffect,useRef} from "react";
+import {ReactComponent} from "*.svg";
+import exp from "constants";
+
+export interface ListItem{
+    id:number;
+}
+
+export interface  ListItems{
+    listItems?:Array<ListItem>;
+}
+
+const ListCreator: FC<ListItems> = React.memo(({listItems}:ListItems) => {
+    let renderItems = useRef<Array<JSX.Element> | undefined>();
+    useEffect(()=>{
+        console.log("Kolekcja listItems została zaktualizowana");
+        renderItems.current = listItems?.map((item,index)=> {
+            return <div key={item.id}>
+                {item.id}
+            </div>;
+        });
+    },[listItems]);
+    console.log("Renderuję komponent ListCreator");
+    return(
+        <React.Fragment>
+            {renderItems.current}
+        </React.Fragment>
+    );
+});
+
+export default ListCreator;
